f5e3ceef16d056612908494fa93512964e59ce01
Freek de Bruijn <freekdb@gmail.com>
2015-02-25 00:02:57 +0100

    Replaced pipe symbols by minus signs (This was confusing the command prompt.)

--- checks.bat
+++ checks.bat
@@ -25,28 +25,28 @@
 
 @echo.
 @echo ================================================================================================================
-@echo Check for Checkstyle issues (mvn checkstyle:checkstyle | report in target\checkstyle-result.xml)
+@echo Check for Checkstyle issues (mvn checkstyle:checkstyle - report in target\checkstyle-result.xml)
 @pause
 @call mvn checkstyle:checkstyle
 @if not "%ERRORLEVEL%" == "0" less target/checkstyle-result.xml
 
 @echo.
 @echo ================================================================================================================
-@echo Check for FindBugs issues (mvn compile findbugs:check | report in target\findbugsXml.xml)
+@echo Check for FindBugs issues (mvn compile findbugs:check - report in target\findbugsXml.xml)
 @pause
 @call mvn compile findbugs:check
 @if not "%ERRORLEVEL%" == "0" less target/findbugsXml.xml
 
 @echo.
 @echo ================================================================================================================
-@echo Check for PMD issues (mvn compile pmd:check | report in target\pmd.xml)
+@echo Check for PMD issues (mvn compile pmd:check - report in target\pmd.xml)
 @pause
 @call mvn compile pmd:check
 @if not "%ERRORLEVEL%" == "0" less target/pmd.xml
 
 @echo.
 @echo ================================================================================================================
-@echo Check for CPD issues (mvn compile pmd:cpd-check | report in target\cpd.xml)
+@echo Check for CPD issues (mvn compile pmd:cpd-check - report in target\cpd.xml)
 @pause
 @call mvn compile pmd:cpd-check
 @if not "%ERRORLEVEL%" == "0" less target/cpd.xml
